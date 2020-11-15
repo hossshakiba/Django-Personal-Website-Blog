6d5d70ea2aa5e424f8f6740dcc947a7f3123f4c7
Hoss <hossshakiba@oulook.com>
2020-11-15 15:04:11 +0330

    added assets

---
+++ static_cdn/static_root/include/twitter/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', '2CCnCiU66KcPkZ0RXbujlr4rT');
+    define('CONSUMER_SECRET', '0chKprtbghB1pNbpRFGKJQrg77j18OEz5hXezWMkJNsCzfEw1D');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '416341865-TCr1ufPx0K03cQZGABfQPMWFZcc65ZcocQzdKBWG');
+    define('ACCESS_SECRET', 'ioikDRzyE2H09psrvJxd414VbCp73QX67qLvIio1IqJqQ');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
---
+++ static_cdn/static_root/js/custom.js
@@ -0,0 +1,9 @@
+/* 
+
+1. Add your custom JavaScript code below
+2. Place the this code in your template:
+
+  
+
+*/
+
